53b1a5149b98aaa0520408a51b3d0ff349ef460f
Mark N <markngsayyao@gmail.com>
2019-03-02 07:20:37 +0800

    Standardize programming style

--- iris_neural_network.py
+++ iris_neural_network.py
@@ -1,10 +1,13 @@
-import sys, os, time
+"""Iris Classifier."""
+import lasagne as nn
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import seaborn as sns
+import sys
 import theano
 import theano.tensor as T
-import lasagne as nn
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+import time
 
 
 def report(text, output_file):
@@ -16,9 +19,11 @@ def report(text, output_file):
 def load_iris_data(filename='iris.txt'):
     with open(filename, 'rb') as f:
         X = [line.split()[0].split(',') for line in f]
-    tag = {'Iris-setosa':0, 'Iris-versicolor':1, 'Iris-virginica':2}
+
+    tag = {'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2}
     Y = [tag[x[4]] for x in X]
     X0, X1, X2 = [], [], []
+
     for x in X:
         if x[4] == 'Iris-setosa':
             X0.append([float(x[0]), float(x[1]), float(x[2]), float(x[3])])
@@ -26,16 +31,20 @@ def load_iris_data(filename='iris.txt'):
             X1.append([float(x[0]), float(x[1]), float(x[2]), float(x[3])])
         elif x[4] == 'Iris-virginica':
             X2.append([float(x[0]), float(x[1]), float(x[2]), float(x[3])])
+
     X0 = np.array(X0).astype('float32')
     X1 = np.array(X1).astype('float32')
     X2 = np.array(X2).astype('float32')
+
     np.random.shuffle(X0)
     np.random.shuffle(X1)
     np.random.shuffle(X2)
+
     X_train = np.concatenate([X0[:25], X1[:25], X2[:25]])
     Y_train = np.concatenate([[0]*25, [1]*25, [2]*25]).astype('int32')
     X_test = np.concatenate([X0[25:], X1[25:], X2[25:]])
     Y_test = np.concatenate([[0]*25, [1]*25, [2]*25]).astype('int32')
+
     return X_train, Y_train, X_test, Y_test
 
 
@@ -44,6 +53,7 @@ def generate_minibatches(data, targets, batch_size=25, stochastic=True):
     idx = np.arange(len(data))
     if stochastic:
         np.random.shuffle(idx)
+
     for k in xrange(0, len(data), batch_size):
         sample = idx[slice(k, k+batch_size)]
         yield data[sample], targets[sample]
@@ -52,9 +62,10 @@ def generate_minibatches(data, targets, batch_size=25, stochastic=True):
 def neural_network():
     net = nn.layers.InputLayer(shape=(None, 4))
     net = nn.layers.DenseLayer(
-        incoming=net, num_units=50, nonlinearity=nn.nonlinearities.rectify)            
+        incoming=net, num_units=50, nonlinearity=nn.nonlinearities.rectify)
     net = nn.layers.DenseLayer(
         incoming=net, num_units=3, nonlinearity=nn.nonlinearities.softmax)
+
     return net
 
 
@@ -64,9 +75,11 @@ def network_functions(net):
     loss = nn.objectives.categorical_crossentropy(output, Y).mean()
     accuracy = T.mean(T.eq(T.argmax(output, axis=1), Y))
     params = nn.layers.get_all_params(net, trainable=True)
-    updates = nn.updates.sgd(loss_or_grads=loss, params=params, learning_rate=.01)
+    updates = nn.updates.sgd(loss_or_grads=loss, params=params,
+        learning_rate=.01)
     train = theano.function([X,Y], [loss, accuracy], updates=updates)
     test = theano.function([X,Y], [loss, accuracy])
+
     return train, test
 
 
@@ -78,23 +91,38 @@ if __name__ == '__main__':
     s = "{:<10}{:<20}{:<20}{:<20}{:<20}{:<20}".format(*headers)
     report(s, './output/iris_details.txt')
     TL, TA, VL, VA = [], [], [], []
+
     for e in range(300):
         start = time.time()
+
         tl, ta, t_batches = 0., 0., 0
         for data, targets in generate_minibatches(X_train, Y_train):
             l, a = train(data, targets)
-            tl += l; ta += a; t_batches += 1;
-        tl /= t_batches; ta /= t_batches
-        TL.append(tl); TA.append(ta)
+            tl += l
+            ta += a
+            t_batches += 1
+
+        tl /= t_batches
+        ta /= t_batches
+        TL.append(tl)
+        TA.append(ta)
+
         vl, va, v_batches = 0., 0., 0
         for data, targets in generate_minibatches(X_test, Y_test):
             l, a = test(data, targets)
-            vl += l; va += a; v_batches += 1;
-        vl /= v_batches; va /= v_batches
-        VL.append(vl); VA.append(va)
+            vl += l
+            va += a
+            v_batches += 1
+
+        vl /= v_batches
+        va /= v_batches
+        VL.append(vl)
+        VA.append(va)
+
         end = time.time() - start
         s = "{:<10}{:<20}{:<20}{:<20}{:<20}{:<20}".format(e+1,tl,ta,vl,va,end)
         report(s, './output/iris_details.txt')
+
     plt.switch_backend('Agg')
     plt.plot([(100 - va*100) for va in VA], c='r', lw=1.5)
     plt.plot([(100 - ta*100) for ta in TA], c='g', lw=1.5)
